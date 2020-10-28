5cc8e9d1f612a279fc9c1a95b028225ae4575eb4
Tareqweb <18103045tareq@gmail.com>
2020-10-28 11:33:26 +0600

    Successfully full project done

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxnQ3psbwKqE7o1uxmzq7d0k99abedq4I",
+  authDomain: "ema-john-simple-f7467.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-f7467.firebaseio.com",
+  projectId: "ema-john-simple-f7467",
+  storageBucket: "ema-john-simple-f7467.appspot.com",
+  messagingSenderId: "786597809588",
+  appId: "1:786597809588:web:16e060cecf8cc3669b36e1"
+};
+export default firebaseConfig;
